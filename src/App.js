bfbc18582a3e4ae9f607ddc30b6af767a3e30fa3
shiref mohammed <shirefmo012345@gmail.com>
2023-04-21 22:26:23 +0200

    updata routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route exact path='/' element={<Feed />} />
-          <Route exact path='/youtube_clone' element={<Feed />} />
+          <Route exact path='/youtube_clone/' element={<Feed />} />
           <Route path='/video/:id' element={<VideoDetail />} />
           <Route path='/channel/:id' element={<ChannelDetail />} />
           <Route path='/search/:searchTerm' element={<SearchFeed />} />
